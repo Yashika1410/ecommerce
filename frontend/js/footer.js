091bcfb9462c538350acef7f89201fc6c49622d0
Yashika1410 <yashikajain1410@gmail.com>
2022-01-04 17:57:00 +0530

    Added HomePage & Footer

---
+++ frontend/js/footer.js
@@ -0,0 +1,30 @@
+const createFooterFun = () => {
+    let footer = document.querySelector('footer');
+
+    footer.innerHTML = `<div class="footer-content">
+            <img src="images/logo.png" class="logo" alt="">
+          
+        </div>
+        <p class="footer-title">about company</p>
+        <p class="info">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat tempore ad suscipit, eos eius
+            quisquam sed optio nisi quaerat fugiat ratione et vero maxime praesentium, architecto minima reiciendis iste quo
+            deserunt assumenda alias ducimus. Ullam odit maxime id voluptates rerum tenetur corporis laboriosam! Cum error ipsum
+            laborum tempore in rerum necessitatibus nostrum nobis modi! Debitis adipisci illum nemo aperiam sed, et accusamus ut
+            officiis. Laborum illo exercitationem quo culpa reprehenderit excepturi distinctio tempore cupiditate praesentium
+            nisi ut iusto, assumenda perferendis facilis voluptas autem fuga sunt ab debitis voluptatum harum eum. Asperiores,
+            natus! Est deserunt incidunt quasi placeat omnis, itaque harum?</p>
+        <p class="info">support emails - help@clothing.com, customersupport@clothing.com</p>
+        <p class="info">telephone - 180 00 00 001, 180 00 00 002</p>
+        <div class="footer-social-container">
+            <div>
+                <a href="#" class="social-link">terms & services</a>
+                <a href="#" class="social-link">privacy page</a>
+            </div>
+            <div>
+                <a href="#" class="social-link">instagram</a>
+                <a href="#" class="social-link">facebook</a>
+                <a href="#" class="social-link">twitter</a>
+            </div>
+        </div>
+        <p class="footer-credit"> Best online store</p>`;}
+        createFooterFun();
